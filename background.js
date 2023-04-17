7aa2a7df8e9e5a78ce6dfc97c365ad604b6251a3
Vincent Le <vle2696@gmail.com>
2023-04-17 15:36:55 -0700

    Add a context menu item (it does nothing for now)

--- background.js
+++ background.js
@@ -1,3 +1,10 @@
 const twitter = 'https://twitter.com'
 
-chrome.action.onClicked.a
+// chrome.action.onClicked.add
+
+chrome.contextMenus.create({
+    id: "coolrefs_download",
+    title: "CoolRef Download Image",
+    contexts: ["image"],
+    // documentUrlPatterns: "https://twitter.com/*"
+})

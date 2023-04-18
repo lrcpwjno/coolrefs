a94d95c1e9f9bca86bc80eb469ff64ddb561b318
Vincent Le <vle2696@gmail.com>
2023-04-17 18:59:06 -0700

    Save input path for use in background, starting on img magic

--- background.js
+++ background.js
@@ -1,10 +1,54 @@
-const twitter = 'https://twitter.com'
+const twitter = 'https://twitter.com';
 
-// chrome.action.onClicked.add
+var path = '/Users/vl1000278391/PycharmProjects/CoolRefs';
+console.log("path is", path);
 
-chrome.contextMenus.create({
-    id: "coolrefs_download",
-    title: "CoolRef Download Image",
-    contexts: ["image"],
-    // documentUrlPatterns: "https://twitter.com/*"
-})
+
+// function download_tweet_images()
+
+// chrome.action.onClicked.addListener(function(info, tab)
+// ;)
+// document.getElementsById('localdestination').onclick = ChangePath
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === "update_path") {
+        path_update = update_path(request.new_path);
+        sendResponse({response: "OK"});
+    };
+});
+
+function update_path(new_path) {
+    path = new_path;
+    console.log("background: new path is", path);
+};
+
+function testFunction() {
+    console.log("you just clicked the context menu item...")
+    console.log(path)
+};
+
+function getPics(info, tab) {
+    currentUrl = info.pageUrl;
+    console.log(currentUrl);
+
+    imgUrl = info.srcUrl;
+    console.log(imgUrl);
+
+    // work your string magic
+};
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    // testFunction()
+    getPics(info, tab);
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: "coolrefs_download",
+        title: "CoolRef Download Image",
+        contexts: ["image"],
+        // documentUrlPatterns: ["https://twitter.com/*"],
+        // onclick: testFunction
+        // we should use info from: pageUrl, linkUrl?
+    });
+});
--- popup.js
+++ popup.js
@@ -1,16 +1,20 @@
-document.getElementById("submitbutton").addEventListener("click", changePath);
+document.getElementById("submitbutton").addEventListener("click", updatePath);
 
 
-function changePath() {
+function updatePath() {
     var path = document.getElementById("localdestination").value;
-    var msg = "path is " + path;
+    var msg = "entered path is " + path;
     console.log(msg);
 
-    // chrome.runtime.sendMessage(
-    //     {
-    //         new_path: path
-    //     },
-        // maybe put a response in case the thing fails to set or something
-    // )
-    
+    let response_promise = chrome.runtime.sendMessage(
+        {
+            message: "update_path",
+            new_path: path
+        },
+        // do we even need this....
+        function(response) {
+            // console.log(response.response)
+        }
+    )
+
 };
